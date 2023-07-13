f4900a07af9bce19dda3dea95c0caf91fcd4f2b6
ansachuk <ansachuk@icloud.com>
2023-07-13 21:25:52 +0300

    change baseURL

--- src/redux/auth/operations.js
+++ src/redux/auth/operations.js
@@ -1,7 +1,7 @@
 import { createAsyncThunk } from "@reduxjs/toolkit";
 import axios from "axios";
 
-axios.defaults.baseURL = "http://localhost:8080/";
+axios.defaults.baseURL = "http://3.126.248.130:8080/";
 
 const setAuthJWTHeader = token => {
 	axios.defaults.headers.common.Authorization = `Bearer ${token}`;
