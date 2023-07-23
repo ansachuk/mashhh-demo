3da588880e3cb29b947c4f80c636da9cd0023057
ansachuk <ansachuk@icloud.com>
2023-07-23 21:25:18 +0300

    change form validation

--- src/schemas/schemas.js
+++ src/schemas/schemas.js
@@ -1,17 +1,44 @@
 import { object, ref, string } from "yup";
 
 const SignupSchema = object().shape({
-	firstName: string().min(3).max(255).nonNullable().required("*required"),
-	lastName: string().min(3).max(255).nonNullable().required("*required"),
-	email: string().email().required("*required"),
+	firstName: string()
+		.min(1, "Password must be at least 1 characters")
+		.max(255, "Password must be at least 255 characters")
+		.matches(/^[A-Za-zА-Яа-я'-]+$/, "Password must contain only uppercase and lowercase Latin and Cyrillic letters, hyphens, and apostrophes")
+		.nonNullable()
+		.required("*required"),
+
+	lastName: string()
+		.min(1, "Password must be at least 1 characters")
+		.max(255, "Password must be at least 255 characters")
+		.matches(/^[A-Za-zА-Яа-я'-]+$/, "Password must contain only uppercase and lowercase Latin and Cyrillic letters, hyphens, and apostrophes")
+		.nonNullable()
+		.required("*required"),
+
+	email: string()
+		.email("Email must be a valid email address")
+		.matches(
+			/^[a-zA-Z0-9!#$%&'*+/=?^_{|}~-]+(?:\.[a-zA-Z0-9!#$%&'*+/=?^_{|}~-]+)*@(?:[a-zA-Z0-9](?:[a-zA-Z0-9-]*[a-zA-Z0-9])?\.)+[a-zA-Z]{2,}$/,
+			"Email must be a valid email address",
+		)
+		.required("*required")
+		.test("username-validation", "Username must meet requirements", value => {
+			const usernameRegex = /^[a-zA-Z0-9!#$%&'*+/=?^_{|}~-]+$/;
+			return usernameRegex.test(value);
+		})
+		.test("domain-validation", "Domain must meet requirements", value => {
+			const domainRegex = /^[a-zA-Z0-9]+([\]{1}[a-zA-Z0-9]+)*\.[a-zA-Z]{2,}$/;
+			return domainRegex.test(value);
+		}),
+
 	password: string()
-		.min(8, "Пароль повинен містити не менше 8 символів")
-		.max(32, "Пароль повинен містити не більше 32 символів")
+		.min(8, "Password must be at least 8 characters")
+		.max(32, "Password must be no more than 32 characters")
 		.matches(
 			/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#&()])[\w!@#&()]+$/,
-			"Пароль повинен містити як мінімум одну цифру, одну маленьку англійську літеру, одну велику англійську літеру, один спецсимвол(!,@,#,&,(,))",
+			"Password must contain at least one digit, one lowercase and uppercase letter, one special character",
 		)
-		.required("Поле пароля є обов'язковим"),
+		.required("*required"),
 
 	confirmPassword: string()
 		.oneOf([ref("password")], "passwords must match")
@@ -19,10 +46,29 @@ const SignupSchema = object().shape({
 });
 
 const LoginSchema = object().shape({
-	email: string().email().required("*required"),
+	email: string()
+		.email("Email must be a valid email address")
+		.matches(
+			/^[a-zA-Z0-9!#$%&'*+/=?^_{|}~-]+(?:\.[a-zA-Z0-9!#$%&'*+/=?^_{|}~-]+)*@(?:[a-zA-Z0-9](?:[a-zA-Z0-9-]*[a-zA-Z0-9])?\.)+[a-zA-Z]{2,}$/,
+			"Email must be a valid email address",
+		)
+		.required("*required")
+		.test("username-validation", "Username must meet requirements", value => {
+			const usernameRegex = /^[a-zA-Z0-9!#$%&'*+/=?^_{|}~-]+$/;
+			return usernameRegex.test(value);
+		})
+		.test("domain-validation", "Domain must meet requirements", value => {
+			const domainRegex = /^[a-zA-Z0-9]+([\]{1}[a-zA-Z0-9]+)*\.[a-zA-Z]{2,}$/;
+			return domainRegex.test(value);
+		}),
+
 	password: string()
-		.min(8, "Пароль повинен містити не менше 8 символів")
-		.max(32, "Пароль повинен містити не більше 32 символів")
+		.min(8, "Password must be at least 8 characters")
+		.max(32, "Password must be no more than 32 characters")
+		.matches(
+			/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#&()])[\w!@#&()]+$/,
+			"Password must contain at least one digit, one lowercase and uppercase letter, one special character",
+		)
 		.required("*required"),
 });
 
